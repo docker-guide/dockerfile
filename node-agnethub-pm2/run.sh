8d168cc3738bfbc40359add576febf2c91b77b89
bdu <bdu@kaikeba.com>
2020-04-27 15:30:39 +0800

    docker镜像

--- node-agnethub-pm2/run.sh
+++ node-agnethub-pm2/run.sh
@@ -3,4 +3,5 @@ parm=$1
 export NODE_LOG_DIR=/tmp
 export ENABLE_NODE_LOG=YES
 DEBUG=* agenthub start config-${parm}.json
+yarn && npm run build:prod
 ENABLE_NODE_LOG=YES NODE_LOG_DIR=/tmp `which pm2-runtime` start ecosystem.config.js --env ${parm}
